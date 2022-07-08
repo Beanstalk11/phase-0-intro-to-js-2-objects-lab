dce80ed86d14231156178bbe3bba46b133b3260d
Nicholas Rogers <nickrogersthegreat@gmail.com>
2022-07-07 18:58:15 -0600

    commit tree 7c8912864061854e4bcf1a7261b203714652f198

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+   name: "Nick",
+   streetAddress: "Paonia",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeUpdate = { ...employee };
+    employeeUpdate[key] = value;
+    return employeeUpdate;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
